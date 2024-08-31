f27ff7f956d1fb0be6d87aad345e36101fe06eab
wakiullah <m.wakiullah1212@gmail.com>
2024-08-31 13:22:38 +0600

    remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import './App.css'
 import Header from './Components/Header'
 import Hero from './Components/Hero'
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Education from './Education';
 import { Link } from 'react-scroll';
 
 
